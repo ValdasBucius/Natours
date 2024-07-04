02c9bd7b454306cef733fd2d28bc96114ab1d01f
Nestinio <valdasbucius1@gmail.com>
2024-07-04 06:09:22 -0700

    CRUD applied

--- app.js
+++ app.js
@@ -28,11 +28,33 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const tour = tours.find((obj) => obj.id === id);
+
+  //   if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: 'Fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours: tour,
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
-  //console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
+
   const newTour = Object.assign({ id: newId }, req.body);
+
   tours.push(newTour);
+
   fs.writeFile(
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
@@ -42,6 +64,43 @@ app.post('/api/v1/tours', (req, res) => {
         data: {
           tours: newTour,
         },
+      });
+    }
+  );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = Number(req.params.id);
+
+  if (id > tours.length) {
+    return res.status(404).json({
+      message: 'There' + 's' + 'no such id',
+    });
+  }
+
+  res.status(200).json({
+    message: 'success',
+  });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const filteredOut = tours.filter((el) => el.id !== id);
+
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'Fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(filteredOut),
+    (err) => {
+      res.status(204).json({
+        status: 'success',
+        data: null,
       });
     }
   );
