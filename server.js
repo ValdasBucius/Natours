3b24af1d2fc0a469db6256f8f47b24d96feb6afb
Nestinio <valdasbucius1@gmail.com>
2024-07-10 10:43:22 -0700

    Eslint/Prettier configuration

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-
 const app = require('./app');
 
+dotenv.config({ path: './config.env' });
+
 const port = process.env.PORT || 8000;
+
 app.listen(port, () => {
   console.log(`App runing on ${port}...`);
 });
